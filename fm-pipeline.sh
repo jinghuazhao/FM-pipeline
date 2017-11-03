80d52adbbb3c1d055926b0f4eb2189238216f709
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-03 10:21:21 +0000

    main program

--- fm-pipeline.sh
+++ fm-pipeline.sh
@@ -72,8 +72,9 @@ join -11 -23 - snp150.txt | \
 sed 's/chr//g' > $rt.input
 sort -k1,1 ${snplist} | \
 join $dir/$(basename $args).input - > $rt.lst
+echo "chr start end pos rsid" > st.bed
 grep -w -f ${snplist} $rt.input | \
-awk -vs=${flanking} '{print $7,$8-s,$8+s, $8, $1}' > st.bed
+awk -vs=${flanking} '{print $7,$8-s,$8+s, $8, $1}' >> st.bed
 if [ $stbed -eq 1 ]; then
    echo "st.bed is generated"
    exit
@@ -87,7 +88,7 @@ parallel -j${threads} -C' ' 'export f=chr{7}_$(({8}-${flanking}))_$(({8}+${flank
          \$0=\$0 \" \" \$7 \":\" \$8 \"_\" a1 \"_\" a2;print}" chr={7} pos={8} s=${flanking} $rt.input |\
          sort -k9,9 > $f.dat'
 echo "--> map/ped"
-cat st.bed | \
+awk 'NR>1' st.bed | \
 parallel -j${threads} --env wd -C' ' '\
     export f=chr{1}_{2}_{3}; \
     awk -f ${FM_location}/files/order.awk $GEN_location/$f.gen > $f.ord;\
@@ -95,13 +96,13 @@ parallel -j${threads} --env wd -C' ' '\
          --missing 0.05 --threshold 0.9 --log $f.log --snp --alleles --chr $(echo $f| \
          cut -d"_" -f1|sed "s/chr//g")'
 echo "--> GWAS .sumstats auxiliary files"
-cat st.bed | \
+awk 'NR>1' st.bed | \
 parallel -j${threads} -C' ' '\
     export f=chr{1}_{2}_{3}; \
     sort -k2,2 $f.map | \
     join -19 -22 $f.dat - | \
     sort -k10,10 > $f.incl'
-cat st.bed | \
+awk 'NR>1' st.bed | \
 parallel -j${threads} --env wd -C' ' '\
     export f=chr{1}_{2}_{3}; \
     awk "{print \$8,\$9,\$3,\$4,\$5,\$6,\$7,\$2,\$1,\$5/\$6}" $f.incl > $f.r; \
@@ -119,13 +120,13 @@ if [ ${sample_to_exclude} == "" ]; then
 else 
    export OPTs="--remove ${sample_to_exclude}"
 fi
-cat st.bed | \
+awk 'NR>1' st.bed | \
 parallel -j${threads} -C' ' '\
     export f=chr{1}_{2}_{3}; \
     plink-1.9 --file $f --missing-genotype N --extract $f.inc ${OPTs} \
     --make-bed --keep-allele-order --a2-allele $f.a 3 1 --out $f'
 echo "JAM, IPD"
-cat st.bed | \
+awk 'NR>1' st.bed | \
 parallel -j${threads} -C' ' '\
     export f=chr{1}_{2}_{3}; \
     plink-1.9 --bfile $f --indep-pairwise 500kb 5 0.80 --maf 0.05 --out $f; \
@@ -134,14 +135,14 @@ parallel -j${threads} -C' ' '\
     plink-1.9 --bfile $f --extract $f.prune.in --keep-allele-order --a2-allele $f.p 3 1 --make-bed --out ${f}p'
 echo "--> finemap, bcor"
 if [ $finemap -eq 1 ]; then
-   cat st.bed
+   awk 'NR>1' st.bed | \
    parallel -j3 -C' ' '\
        export f=chr{1}_{2}_{3}; \
        ldstore --bcor $f.bcor --bplink $f --n-threads ${threads}; \  
        ldstore --bcor $f.bcor --merge ${threads}; \
        ldstore --bcor $f.bcor --matrix $f.ld --incl_variants $f.incl_variants; \
        sed -i -e "s/  */ /g; s/^ *//; /^$/d" $f.ld'
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j3 -C' ' '\ 
        export f=chr{1}_{2}_{3}; \
        grep -w -f $f.prune.in $f.z > ${f}p.z; \
@@ -150,39 +151,39 @@ if [ $finemap -eq 1 ]; then
        ldstore --bcor ${f}p.bcor --matrix ${f}p.ld; \
        sed -i -e "s/  */ /g; s/^ *//; /^$/d" ${f}p.ld'
    echo "z;ld;snp;config;log;n-ind" > finemap.cfg
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C ' ' '\
        export f=chr{1}_{2}_{3}; \
        sort -k7,7n $f.r | \
        tail -n1|cut -d" " -f7| \
        awk -vf=$f "{print sprintf(\"%s.z;%s.ld;%s.snp;%s.config;%s.log;%d\",f,f,f,f,f,int(\$1))}" >> finemap.cfg'
    finemap --sss --in-files finemap.cfg --n-causal-max 5 --corr-config 0.9
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}; \
        R --no-save < ${FM_location}/files/finemap-check.R > $f.chk'
    sed 's/\./p\./g' finemap.cfg > finemapp.cfg
    finemap --sss --in-files finemapp.cfg --n-causal-max 5 --corr-config 0.9
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}p; \
        R --no-save < ${FM_location}/files/finemap-check.R > $f.chk'
 fi
 echo "--> JAM"
 if [ $JAM -eq 1 ]; then
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}p; \
        R --no-save <${FM_location}/files/JAM.R > ${f}.log'
 fi
 if [ $CAVIAR -eq 1 ]; then
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}; \
        CAVIAR -z ${f}.z -l ${f}.ld -r 0.9 -o ${f}'
 fi
 if [ $CAVIARBF -eq 1 ]; then
-   cat st.bed | \
+   awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}; \
        caviarbf -z ${f}.z -r ${f}.ld -n $N -t 0 -a 0.1 -c 3 --appr -o ${f}.caviarbf'
