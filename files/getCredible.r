3414138728e7ab9c2ae0f3e320dab1b04bebd030
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-03 14:53:33 +0000

    auxiliary files

--- files/getCredible.r
+++ files/getCredible.r
@@ -2,33 +2,32 @@
 # 3-11-2017 MRC-Epid JHZ
 
 args <- commandArgs(trailingOnly = TRUE)
-options(scipen=20,width=150)
+options(scipen=20,width=200)
 getCredibleSNP <- function(snp, logProb, threshold=0.99)
 {
   prob <- exp(logProb) 
   prob_normed <- prob/sum(prob)
   prob_cumsum <- cumsum(sort(prob_normed, decreasing=TRUE))
-  nSNP <- as.numeric(which.max(prob_cumsum>threshold ))
-  if(nSNP<=0){ nSNP=1 }
+  nSNP <- as.numeric(which.max(prob_cumsum > threshold ))
+  if(nSNP<=0) nSNP=1
   credible_set <- snp[order(prob_normed, decreasing=TRUE)[1:nSNP]]
   select <- rep(FALSE, length(snp))
-  select[order(prob_normed, decreasing=TRUE)[1:nSNP]] <- T 
+  select[order(prob_normed, decreasing=TRUE)[1:nSNP]] <- TRUE
   ret <- list(nSNP = nSNP, 
 	 prob_normed = prob_normed, 
          prob_cumsum = prob_cumsum[rank(-prob_normed, ties.method="random")], 
          credible_set=credible_set, 
-         select=select
- )
+         select=select)
 }
 bed <- read.table("st.bed", as.is=TRUE, col.names=("chr","start","end","pos","rsid","r"), skip=args[1]+1, nrows=1)
 f <- Sys.getenv("f")
 dat <- read.table(paste0(f,".r"), as.is=TRUE,
        col.names=c("chr","pos","A","B","Effect","StdErr","TOTALSAMPLESIZE","SNP",'SNPID',"z"))
 dat <- within(dat, P=2*pnorm(abs(z),lower.tail=FALSE))
-select <- with(dat,P)<0.1
+select <- with(dat, P)<0.1
 max_SCZ_P <- min(dat$P[select])
 max_SCZ_snp <- as.character(dat$SNP[select][which.min(dat$P[select])])
-ret <- getCredibleSNP(as.character(dat$SNP[select]), qchisq(dat$P[select], 1, low=F)/2)
+ret <- getCredibleSNP(as.character(dat$SNP[select]), qchisq(dat$P[select], 1, low=FALSE)/2)
 inCredible <- rep(NA, length(dat$P))
 inCredible[match( dat$SNP[select], dat$SNP )] <- 0
 inCredible[match( dat$SNP[select][ret$select], dat$SNP )] <- 1
@@ -38,4 +37,4 @@ prob_cumsum <- rep(NA, length(dat$P))
 prob_cumsum[match(dat$SNP[select], dat$SNP )] <- ret$prob_cumsum
 result <- cbind(dat, inCredible=inCredible, probNorm=prob_norm, cumSum=prob_cumsum)
 write.table(subset(result,!is.na(inCredible)&!is.na(prob_norm)&!is.na(prob_cumsum)), 
-            file=paste0(f,".cre"), sep="\t", quote=F, col.names=T, row.names=F)
+            file=paste0(f,".cre"), sep="\t", quote=F, col.names=TRUE, row.names=FALSE)
