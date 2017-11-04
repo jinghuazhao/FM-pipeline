787b72e918d2bb525e997b07add3292101b403ff
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-04 17:54:09 +0100

    main program

--- fm-pipeline.sh
+++ fm-pipeline.sh
@@ -163,12 +163,35 @@ if [ $finemap -eq 1 ]; then
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}; \
        R --no-save < ${FM_location}/files/finemap-check.R > $f.chk'
+   echo "snpid region index snp_prob snp_log10bf rsid" > finemap.K20
+   awk 'NR>1' st.bed | \
+   parallel -j${threads} -C' ' '\
+       export f=chr{1}_{2}_{3}; \
+       cut -d" " -f10,11 $f.r > $f.tmp; \
+       awk "(NR>1&&\$3>0.8&&\$4>1.3){print ENVIRON["f"], \$0}" $f.snp | \
+       sort -k3,3 | \
+       join -13 -22 - $f.tmp >> finemap.K20'
+   echo "chr pos log10BF prob snpid rsid region" > finemap.dat
+   awk '(NR>1){snpid=$1;gsub(/:|_/," ",$1);split($1,a," ");print a[1],a[2],$5,$4,snpid,$6,$2}' finemap.K20 | \
+   sort -k1,1n -k2,2n >> finemap.dat
    sed 's/\./p\./g' finemap.cfg > finemapp.cfg
    finemap --sss --in-files finemapp.cfg --n-causal-max 5 --corr-config 0.9
    awk 'NR>1' st.bed | \
    parallel -j${threads} -C' ' '\
        export f=chr{1}_{2}_{3}p; \
        R --no-save < ${FM_location}/files/finemap-check.R > $f.chk'
+   # The pruned data
+   echo "snpid region index snp_prob snp_log10bf rsid" > finemapp.K20
+   awk 'NR>1' | \
+   parallel -j${threads} -C' ' '\
+       export f=chr{1}_{2}_{3}; \
+       cut -d" " -f10,11 $f.r > $f.tmp; \
+       awk "(NR>1&&\$3>0.8&&\$4>1.3){print ENVIRON["f"], \$0}" ${f}p.snp | \
+       sort -k3,3 | \
+       join -13 -22 - $f.tmp >> finemapp.K20'
+   echo "chr pos log10BF prob snpid rsid region" > finemapp.dat
+   awk '(NR>1){snpid=$1;gsub(/:|_/," ",$1);split($1,a," ");print a[1],a[2],$5,$4,snpid,$6,$2}' finemapp.K20 | \
+   sort -k1,1n -k2,2n >> finemapp.dat
 fi
 echo "--> JAM"
 if [ $JAM -eq 1 ]; then
