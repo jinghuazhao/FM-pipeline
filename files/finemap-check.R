ef09485e26a56984ada3aa4c5321e2852791a716
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-05 06:53:21 +0100

    auxiliary files

--- files/finemap-check.R
+++ files/finemap-check.R
@@ -4,7 +4,7 @@ options(width=200)
 f <- Sys.getenv("f")
 z <- read.table(paste0(f, ".z"), col.names=c("snp","z"))
 snp <- read.table(paste0(f, ".snp"), header=TRUE)
-index <- with(subset(snp,snp_log10bf>0), index)
+index <- with(subset(snp, snp_log10bf>0), index)
 ld <- read.table(paste0(f, ".ld"))
 snp[1:length(index), ]
 z[index, ]
