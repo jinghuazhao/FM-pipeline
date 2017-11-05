adf3a62d36f183eba09fba946d57c09a381680a2
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-05 08:24:21 +0000

    auxiliary files

---
+++ files/finemap-xlsx.R
@@ -0,0 +1,26 @@
+# 5-11-2017 MRC-Epid JHZ
+
+options(digits=3, scipen=20, width=200)
+
+library(openxlsx)
+bed <- read.table("st.bed", as.is=TRUE, header=TRUE)
+for(r in nrows(bed))
+{
+   rsid <- with(bed, rsid)[r]
+   f <- paste(bed[r,1:3], sep="_")
+   xlsx <- paste0("chr", f, "-", rsid, ".xlsx")
+   unlink(xlsx, recursive = FALSE, force = FALSE)
+   wb <- createWorkbook(xlsx)
+   if(file.exists(paste0(rsid, ".pdf")))
+   {
+     addWorksheet(wb, paste0(rsid, "_plot"))
+     insertImage(wb, paste0(rsid, "_plot"), paste0(rsid, "-000001.png"), width=18, height=12)
+   }
+   snp <- read.table(paste0("chr", f, ".snp"), as.is=TRUE, header=TRUE)
+   addWorksheet(wb, paste0(f, ".snp"))
+   writeDataTable(wb, paste0(f, ".snp"), snp)
+   config <- read.table(paste0("chr", f, ".config"), as.is=TRUE, header=TRUE)
+   addWorksheet(wb, paste0(f, ".cfg"))
+   writeDataTable(wb, paste0(f, ".cfg"), config)
+   saveWorkbook(wb, file=xlsx, overwrite=TRUE)
+}
