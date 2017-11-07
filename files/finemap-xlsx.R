269eda89b77441d37f93e8ab3d855396ac05fc57
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-07 09:03:52 +0000

    auxiliary files

--- files/finemap-xlsx.R
+++ files/finemap-xlsx.R
@@ -1,4 +1,4 @@
-# 5-11-2017 MRC-Epid JHZ
+# 7-11-2017 MRC-Epid JHZ
 
 options(digits=3, scipen=20, width=200)
 
@@ -11,7 +11,7 @@ for(r in 1:nrow(bed))
    xlsx <- paste0("chr", f, "-", rsid, ".xlsx")
    unlink(xlsx, recursive = FALSE, force = TRUE)
    wb <- createWorkbook(xlsx)
-   if(file.exists(paste0(rsid, ".pdf")))
+   if(file.exists(paste0(rsid, "-000001.png")))
    {
      addWorksheet(wb, paste0(rsid, "_plot"))
      insertImage(wb, paste0(rsid, "_plot"), paste0(rsid, "-000001.png"), width=18, height=12)
