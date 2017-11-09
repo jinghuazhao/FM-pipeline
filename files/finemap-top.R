64784c0d27e911425b1be232c03223c0a2820c8c
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-09 16:12:11 +0000

    auxiliary files

---
+++ files/finemap-top.R
@@ -0,0 +1,15 @@
+# 9-11-2017 MRC-Epid JHZ
+
+options(digits=3, scipen=20, width=200)
+
+library(openxlsx)
+p <- Sys.getenv("p")
+xlsx <- paste0("finemap-top", p, ".xlsx")
+wb <- createWorkbook(xlsx)
+snp <- read.table(paste0("snp", p, ".dat"), as.is=TRUE, header=TRUE, sep=" ")
+addWorksheet(wb, "snp")
+writeDataTable(wb, "snp", snp)
+config <- read.table("config", p", ".dat"), as.is=TRUE, header=TRUE, sep=" ")
+addWorksheet(wb, "config")
+writeDataTable(wb, "config", subset(config,config_prob>0.4))
+saveWorkbook(wb, file=xlsx, overwrite=TRUE)
