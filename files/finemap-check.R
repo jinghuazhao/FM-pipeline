bf11389015b583667d46e5dd41eb2ce396f319ad
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-11-11 18:02:56 +0000

    auxiliary files

--- files/finemap-check.R
+++ files/finemap-check.R
@@ -1,6 +1,6 @@
-# 5-11-2017 MRC-Epid JHZ
+# 11-11-2017 MRC-Epid JHZ
 
-options(width=200)
+options(echo=FALSE, width=200)
 
 f <- Sys.getenv("f")
 cat(f, "\n")
@@ -8,6 +8,7 @@ z <- read.table(paste0(f, ".z"), col.names=c("snp","z"))
 snp <- read.table(paste0(f, ".snp"), header=TRUE)
 index <- with(subset(snp, snp_log10bf>0), index)
 ld <- read.table(paste0(f, ".ld"))
+options(echo=TRUE)
 snp[1:length(index), ]
 z[index, ]
 ld[index, index]
