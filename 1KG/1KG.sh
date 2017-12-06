f01b720a5c10da4505316bad32611619b91c7529
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2017-12-06 10:38:59 +0000

    1000 Genomes reference panel

--- 1KG/1KG.sh
+++ 1KG/1KG.sh
@@ -1,4 +1,4 @@
-#2-12-2017 MRC-Epid JHZ
+# 6-12-2017 MRC-Epid JHZ
 
 wget -qO- https://data.broadinstitute.org/alkesgroup/FUSION/LDREF.tar.bz2 | tar xfj - --strip-components=1
 seq 22|awk -vp=1000G.EUR. '{print p $1 ".bed " p $1 ".bim " p $1 ".fam"}' > merge-list
@@ -10,7 +10,6 @@ stata <<END
   sort rsid
   gzsave EUR, replace
   insheet chr rsid m pos A1 A2 using EUR.bim, case clear
-  gen RSnum=rsid
   gen info=1
   gen type=2
   sort rsid
