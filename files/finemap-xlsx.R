ddda2b13923396dc70db1a71cae7823d8f5a1125
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-13 13:28:18 +0000

    auxiliary files

--- files/finemap-xlsx.R
+++ files/finemap-xlsx.R
@@ -1,4 +1,4 @@
-# 3-11-2018 MRC-Epid JHZ
+# 13-1-2018 MRC-Epid JHZ
 
 options(digits=3, scipen=20, width=200)
 
@@ -12,11 +12,6 @@ for(r in 1:nrow(bed))
    xlsx <- paste0("chr", f, "-", rsid, ".xlsx")
    unlink(xlsx, recursive = FALSE, force = TRUE)
    wb <- createWorkbook(xlsx)
-   if(file.exists(paste0(rsid, "-000001.png")))
-   {
-     addWorksheet(wb, paste0(rsid, "_plot"))
-     insertImage(wb, paste0(rsid, "_plot"), paste0(rsid, "-000001.png"), width=18, height=12)
-   }
    snp <- read.table(paste0("chr", f, ".snp"), as.is=TRUE, header=TRUE)
    addWorksheet(wb, paste0(f, ".snp"))
    writeDataTable(wb, paste0(f, ".snp"), snp)
---
+++ files/lz.R
@@ -0,0 +1,22 @@
+# 13-1-2018 MRC-Epid JHZ
+
+options(digits=3, scipen=20, width=200)
+
+lz <- Sys.getenv("LocusZoom")
+library(openxlsx)
+bed <- read.table("st.bed", as.is=TRUE, header=TRUE)
+xlsx <- paste0("lz.xlsx")
+unlink(xlsx, recursive = FALSE, force = TRUE)
+wb <- createWorkbook(xlsx)
+for(r in 1:nrow(bed))
+{
+   rsid <- with(bed, rsid)[r]
+   f <- paste(bed[r,1], bed[r,2],bed[r,3],sep="_")
+   if(file.exists(paste0(rsid, "-000001.png")))
+   {
+     addWorksheet(wb, paste0(rsid, "_plot"))
+     insertImage(wb, paste0(rsid, "_plot"), paste0(rsid, "-000001.png"), width=18, height=12)
+   }
+}
+saveWorkbook(wb, file=xlsx, overwrite=TRUE)
+
