7d797793c8fc004f75af33fa492e5ae76fb80659
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-14 10:33:08 +0000

    auxiliary files

--- files/finemap.R
+++ files/finemap.R
@@ -1,4 +1,4 @@
-# 13-1-2018 MRC-Epid JHZ
+# 14-1-2018 MRC-Epid JHZ
 
 options(digits=3, scipen=20, width=500)
 
@@ -7,21 +7,18 @@ cat(f, "\n")
 z <- read.table(paste0(f, ".z"), as.is=TRUE, col.names=c("snp","z"))
 ld <- read.table(paste0(f, ".ld"))
 snp <- read.table(paste0(f, ".snp"), as.is=TRUE, header=TRUE)
-index <- with(subset(snp, snp_prob>0.01), index)
 config <- read.table(paste0(f,".config"),as.is=TRUE,header=TRUE)
 subset(snp, snp_prob>0.01)
 subset(config,config_prob>0.01)
-chk <- cbind(z[index, ], snp[1:length(index), c(3,4)], ld[index, index])
+id <- with(subset(snp, snp_prob>0.01), index)
+chk <- cbind(z[id, ], with(subset(snp,index%in%id), c(snp_prob,snp_log10bf)), ld[id, id])
 chk
 
-## Adapted from code by Ji Chen 
-## Find number of configs that account for 75% probability mass
+## Code from Ji Chen for number of configs that account for 75% probability and variants in the credible set
 end <- which( cumsum( config$config_prob ) >= 0.75 )[ 1 ]
-## Get variants in the credible set
 credible_set <- unique( strsplit( paste( config$config[ seq( end ) ], collapse = ',' ), split = ',' )[[ 1 ]] )
 head(config, end)
-index <- with(subset(snp, snp%in%credible_set&snp_prob>0), index)
-cs <- cbind(z[index, ], snp[index, c(3,4)])
+cs <- subset(cbind(z,snp), (index%in%credible_set)&snp_prob>0)
 cs
 
 library(openxlsx)
