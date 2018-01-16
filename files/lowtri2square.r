78d00417cca3a4998c78a37cc1ead3bad949396f
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-16 14:42:23 +0000

    auxiliary files

--- files/lowtri2square.r
+++ files/lowtri2square.r
@@ -1,8 +1,11 @@
 # 16-1-2018 MRC-Epid JHZ
 
 f <- Sys.getenv("f")
-z <- gzfile(paste0(f,".magic.ld.gz"))
-m <- read.table(z, fill=TRUE, col.names=paste0("v",))
-m[upper.tri(m)] <- t(m)[upper.tri(m)]
-write.table(m,file=paste0(f,".ld"),col.names=FALSE,row.names=FALSE,quote=FALSE)
 
+m <- read.table(paste0(f,".magic.map"),skip=11,header=TRUE,as.is=TRUE)
+nr <- nrow(m)
+z <- gzfile(paste0(f,".magic.ld.gz"))
+ld <- read.table(z, fill=TRUE, col.names=paste0("v",1:nr))
+ld[upper.tri(ld)] <- t(ld)[upper.tri(ld)]
+diag(ld) <- 1
+write.table(ld,file=paste0(f,".ld"),col.names=FALSE,row.names=FALSE,quote=FALSE)
