f8a78b89d9e632503b5baf0413fa662c0d56c9eb
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-01-21 15:17:42 +0000

    main program

--- fmp.sh
+++ fmp.sh
@@ -264,7 +264,8 @@ if [ $fgwas -eq 1 ]; then
    # -/+ flanking position
    export flanking=250000
    awk -vfl=${flanking} 'NR>1{l=$2;u=$3;print $5,$1,$4,l,u,NR}' st.bed | \
-   sort -k1,1 | parallel -j${threads} --env FM_location --env fgwas_location_1kg -C' ' '
+   sort -k1,1 > fgwas.snplist
+   cat fgwas.splist | parallel -j${threads} --env FM_location --env fgwas_location_1kg -C' ' '
        export f=chr{2}_{4}_{5}; \
        awk -vsn={6} -f $FM_location/files/fgwas.awk $f.r | \
        sort -k3,3 | \
