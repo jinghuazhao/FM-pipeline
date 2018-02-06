ca779b5917cf4cfa77d0d1ce2d24eca1c1c73d42
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-02-06 20:42:35 +0000

    auxiliary files

--- files/gcta-jam-finemap.R
+++ files/gcta-jam-finemap.R
@@ -1,4 +1,4 @@
-# 1-2-2018 MRC-Epid JHZ
+# 6-2-2018 MRC-Epid JHZ
 
 options(scipen=20, width=200, rgl.useNULL=TRUE)
 
@@ -6,7 +6,9 @@ if(file.exists("gcta-slct.csv")&file.exists("jam.cs"))
 {
 
   slct <- read.csv("gcta-slct.csv",as.is=TRUE)
+  names(slct)[16] <- "rsid1"
   cs <- read.table("jam.cs",header=TRUE,as.is=TRUE)
+  names(cs)[3] <- "rsid2"
   require(openxlsx)
   xlsx <- "gcta-jam-finemap.xlsx"
   wb <- createWorkbook(xlsx)
@@ -22,7 +24,7 @@ if(file.exists("gcta-slct.csv")&file.exists("jam.cs"))
     cat(stbed[i,"r"],r,"\n")
     slct.r <- subset(slct,region==r)
     cs.r <- subset(cs,region==r)
-    slct_cs.r <- qpcR:::cbind.na(slct.r[c("region","SNP","rsid","pJ")],cs.r[c("snpid","PostProb","BF")])
+    slct_cs.r <- qpcR:::cbind.na(slct.r[c("region","SNP","rsid1","pJ")],cs.r[c("snpid","rsid2","PostProb","BF")])
     addWorksheet(wb, r)
     writeDataTable(wb, r, slct_cs.r)
     snplist <- unique(sort(c(with(slct.r, SNP),with(cs.r, snpid))))
