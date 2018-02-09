00fc0359546c4bc549f4bc8868fe9abb2a5bff31
jinghuazhao <jinghua.zhao@mrc-epid.cam.ac.uk>
2018-02-09 12:51:26 +0000

    auxiliary files

---
+++ files/JAM-cs.R
@@ -0,0 +1,36 @@
+# 9-2-2018 MRC-Epid JHZ
+
+options(scipen=20, width=200)
+
+# collate all Credible sets
+cs <- data.frame()
+stbed <- read.table("st.bed",as.is=TRUE,header=TRUE)
+for(i in seq(nrow(stbed)))
+{
+  r <- paste0("chr",stbed[i,1],"_",stbed[i,2],"_",stbed[i,3])
+  f <- paste0(r,"p.cs")
+  if(file.exists(f))
+  {
+    t <- read.table(f,as.is=TRUE,header=TRUE)
+    cs <- rbind(cs,data.frame(region=r,t))
+  }
+}
+
+# split snpid
+cs <- within(cs, {
+   sp <- lapply(lapply(snpid, function(x) strsplit(x,":|_")),unlist)
+   Chr <- Pos <- NA
+})
+
+# obtain positions
+cs <- within(cs, {
+   for(i in seq(nrow(cs))) {
+      Chr[i] <- as.integer(sp[[i]][1])
+      Pos[i] <- as.integer(sp[[i]][2])
+   }
+})
+
+# order by positions
+ord <- with(cs, order(Chr,Pos))
+cs <- within(cs[ord,],{i <- ssnpid <- sp <- NULL})
+write.table(cs, file="jam.cs", row.names=FALSE, quote=FALSE)
--- files/ld.R
+++ files/ld.R
@@ -1,7 +1,7 @@
 # 9-2-2018 MRC-Epid JHZ
 
 library(openxlsx)
-xlsx <- "gcta-jam.xlsx"
+xlsx <- "gcta-jam-finemap.xlsx"
 ld <- read.table("ld.txt",as.is=TRUE,fill=TRUE,header=TRUE)
 wb <- loadWorkbook(xlsx)
 addWorksheet(wb,"LD")
