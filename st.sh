bfbe08ca160e121bf6704c56ab259a824e435bd1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-07-17 12:39:17 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,19 @@
+# 17-7-2018 MRC-Epid JHZ
+
+git add .gitignore
+git commit -m "Don't process these"
+git add files
+git commit -m "auxiliary files"
+git add fmp.sh
+git commit -m "main program"
+git add gcta-slct.sh
+git commit -m "GCTA conditional/joint analysis"
+git add vegas2v2.sh
+git commit -m "VEGASv2 on significant genes"
+git add 1KG
+git commit -m "1000 Genomes reference panel"
+git add README.*
+git commit -m "README"
+git add st.sh
+git commit -m "st.sh"
+git push
