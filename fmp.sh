b779bb555ae2e0b8d1c6b8eb6417c317fe0defa8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-07-26 11:55:35 +0100

    main program

--- fmp.sh
+++ fmp.sh
@@ -12,29 +12,31 @@ export JAM=1
 export LocusZoom=0
 export fgwas=0
 export finemap=1
-export fgwas_location_1kg=/genetics/data/fgwas/1000-genomes-genetic-maps
+# parallel processes when available
+export threads=1
+# Default location and reference data
 export FM_location=/genetics/bin/FM-pipeline
-
-export wd=$(pwd)
 # GEN files named chr{chr}_{start}_{end}.gen.gz
 export GEN_location=$FM_location/1KG/LD-blocks
 # sample file
 export sample_file=$FM_location/1KG/EUR.sample
-# wholegenome genotype file
-export HRC=/gen_omics/data/EPIC-Norfolk/HRC/binary_ped
-export bfile=$HRC/HRC
-export remove_sample=$HRC/exclude.id
-export exclude_snp=$HRC/exclude.snps
+# Complementary files for fgwas
+export fgwas_location_1kg=/genetics/data/fgwas/1000-genomes-genetic-maps
 
-## OTHER OPTIONS
+## OTHER SETTINGS
 
 export PATH=/genetics/bin:/usr/local/bin:$PATH:/genetics/data/software/bin
 export R_LIBS=/usr/local/lib/R/site-library/:/genetics/bin/R:/usr/local/lib64/R/library:/genetics/data/software/R
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64/R/lib:/genetics/data/software/lib
 
-export threads=1
+# Other possibilities to calculate LD
 export LD_MAGIC=0
 export LD_PLINK=0
+# wholegenome genotype file
+export HRC=/gen_omics/data/EPIC-Norfolk/HRC/binary_ped
+export bfile=$HRC/HRC
+export remove_sample=$HRC/exclude.id
+export exclude_snp=$HRC/exclude.snps
 # nonempty value to skip parallel sessions for data handling and go directly to analysis
 export dry_run=
 export OPTs=""
@@ -53,6 +55,7 @@ fi
 export args=$1
 if [ $(dirname $args) == "." ]; then dir=$(pwd)/$(basename $args).out; else dir=$args.out; fi
 if [ ! -d $dir ]; then mkdir -p $dir; fi
+export wd=$(pwd)
 cd $dir
 ln -sf $wd/$args
 export rt=$dir/$(basename $args)
