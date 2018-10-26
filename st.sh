87b3cc7e46aa23a207565eea6897ebf8005d0fce
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-26 10:28:30 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,12 +1,12 @@
-# 10-9-2018 MRC-Epid JHZ
+# 26-10-2018 MRC-Epid JHZ
 
 git add .gitignore
 git commit -m "Don't process these"
 git add files
 git commit -m "auxiliary files"
-git add FMP.ini
+git add fmp.ini
 git commit -m "settings"
-git add fmp.sh
+git add fmp.sh fmp.subs
 git commit -m "main program"
 git add gcta-slct.sh
 git commit -m "GCTA conditional/joint analysis"
