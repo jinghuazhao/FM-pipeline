41de08b6ce5dcd235c296c795ecf8751c0148920
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-04 15:02:36 +0000

    auxiliary files

--- files/finemap.R
+++ files/finemap.R
@@ -1,4 +1,4 @@
-# 26-7-2018 JHZ
+# 4-1-2019 JHZ
 
 options(digits=3, scipen=20, width=500)
 
@@ -15,16 +15,6 @@ ld[id,id][upper.tri(ld[id,id])] <- NA
 chk <- cbind(z[id, ], with(subset(snp,index%in%id), c(prob,log10bf)), ld[id, id])
 chk
 
-## Code from Ji Chen for number of configs that account for % probability and variants in the credible set
-end <- which( cumsum( config$prob ) >= 0.75 )[ 1 ]
-end
-credible_set <- unique( strsplit( paste( config$config[ seq( end ) ], collapse = ',' ), split = ',' )[[ 1 ]] )
-head(config, end)
-snplist <- with(subset(snp,(snp%in%credible_set)&prob>0),snp)
-length(snplist)
-cs <- cbind(subset(z,snp%in%snplist),with(subset(snp, snp%in%snplist),cbind(prob,log10bf)))
-cs
-
 library(openxlsx)
 xlsx <- paste0(f, ".xlsx")
 unlink(xlsx, recursive = FALSE, force = TRUE)
@@ -35,6 +25,18 @@ addWorksheet(wb, paste0(f, ".cfg"))
 writeDataTable(wb, paste0(f, ".cfg"), config)
 addWorksheet(wb, paste0(f, ".chk"))
 writeDataTable(wb, paste0(f, ".chk"), chk)
-addWorksheet(wb, paste0(f, ".cs"))
-writeDataTable(wb, paste0(f, ".cs"), cs)
+CredibleSet <- function()
+{
+## Code from Ji Chen for number of configs that account for % probability and variants in the credible set
+  end <- which( cumsum( config$prob ) >= 0.75 )[ 1 ]
+  end
+  credible_set <- unique( strsplit( paste( config$config[ seq( end ) ], collapse = ',' ), split = ',' )[[ 1 ]] )
+  head(config, end)
+  snplist <- with(subset(snp,(snp%in%credible_set)&prob>0),snp)
+  length(snplist)
+  cs <- cbind(subset(z,snp%in%snplist),with(subset(snp, snp%in%snplist),cbind(prob,log10bf)))
+  cs
+  addWorksheet(wb, paste0(f, ".cs"))
+  writeDataTable(wb, paste0(f, ".cs"), cs)
+}
 saveWorkbook(wb, file=xlsx, overwrite=TRUE)
