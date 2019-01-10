e64a50bc4d63c807d8a6078cc2de9b9ae0a2f2e8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-10 14:24:30 +0000

    1000 Genomes reference panel

---
+++ 1KG/lz-1.4.sh
@@ -0,0 +1,10 @@
+# 10-1-2019 JHZ
+
+sbatch --wait lz-1.4.sb
+
+for i in `seq 22`; do echo EUR1KG-$i; done > EUR.list
+plink --merge-list EUR.list --make-bed --out EUR
+
+qctool -filetype binary_ped -g EUR.bed -ofiletype gen -og EUR.gen.gz 
+
+sbatch --wait extract.sb
