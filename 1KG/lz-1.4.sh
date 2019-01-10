aef81ece779d9dcfc08bb7d089f117974ba222b1
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-10 19:41:35 +0000

    1000 Genomes reference panel

--- 1KG/lz-1.4.sh
+++ 1KG/lz-1.4.sh
@@ -29,39 +29,36 @@ stata <<END
   drop _merge
   gen maf=cond(FreqA2<=0.5, FreqA2, 1-FreqA2)
   sort chr pos snpid
-  gen MAC=2*489*maf
+  gen MAC=2*503*maf
   rename FreqA2 exp_freq_a1
   order snpid pos exp_freq_a1 info type
   gzsave SNPinfo, replace
-END
 
-// extract GEN file for each LD region in 1000G data
+  set more off
 
-stata <<END
-set more off
-
-gzuse SNPinfo.dta.gz, clear
-tempfile f0
-forval k=1/22 {
-   preserve
-   keep if chr==`k'
-   save `f0', replace
-   import delimited using EUR.bed, asdouble clear
-   keep chr start end
-   drop if end<=start
-   gen chr=substr(chrom,4,2)
-   keep if chr==`k'
-   drop chr
-   sort start
-   count
-   local nclus=r(N)
-   merge 1:1 _n using `f0', nogen
-   forval j=1/`nclus' {
-      local lowr=start[`j']
-      local uppr=end[`j']
-      local f="chr`k'_`lowr'_`uppr'"
-      outsheet snpid pos exp_freq_a1 info type rsid if pos>=`lowr' & pos<=`uppr' using LocusZoom-1.4/`f'.info, names noquote replace nolab delim(" ")
-   }
-   restore
-}
+  gzuse SNPinfo.dta.gz, clear
+  tempfile f0
+  forval k=1/22 {
+     preserve
+     keep if chr==`k'
+     save `f0', replace
+     import delimited using EUR.bed, asdouble clear
+     keep chrom start end
+     drop if end<=start
+     gen chr=substr(chrom,4,2)
+     destring chr, replace
+     keep if chr==`k'
+     drop chr
+     sort start
+     count
+     local nclus=r(N)
+     merge 1:1 _n using `f0', nogen
+     forval j=1/`nclus' {
+        local lowr=start[`j']
+        local uppr=end[`j']
+        local f="chr`k'_`lowr'_`uppr'"
+        outsheet snpid pos exp_freq_a1 info type rsid if pos>=`lowr' & pos<=`uppr' using LocusZoom-1.4/`f'.info, names noquote replace nolab delim(" ")
+     }
+     restore
+  }
 END
