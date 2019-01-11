ab883cae2134a2566156f19693bc992b03d8ecce
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-01-11 10:40:16 +0000

    1000 Genomes reference panel

--- 1KG/FUSION.do
+++ 1KG/FUSION.do
@@ -1,3 +1,24 @@
+// generate FUSION.dta.gz
+
+insheet rsid FreqA2 using EUR.dat, case
+sort rsid
+gzsave EUR, replace
+insheet chr rsid m pos A1 A2 using EUR.bim, case clear
+gen RSnum=rsid
+gen info=1
+gen type=2
+sort rsid
+gzmerge using EUR
+gen snpid=string(chr)+":"+string(pos,"%12.0f")+"_"+cond(A1<A2,A1,A2)+"_"+cond(A1<A2,A2,A1)
+sort chr pos
+drop _merge
+gen maf=cond(FreqA2<=0.5, FreqA2, 1-FreqA2)
+sort chr pos snpid
+gen MAC=2*489*maf
+rename FreqA2 exp_freq_a1
+order snpid pos exp_freq_a1 info type
+gzsave FUSION, replace
+
 // extract GEN file for each LD region in 1000G data
 
 set more off
--- 1KG/FUSION.sh
+++ 1KG/FUSION.sh
@@ -6,23 +6,5 @@ seq 22|awk -vp=1000G.EUR. '{print p $1}' > merge-list
 plink-1.9 --merge-list merge-list --make-bed --out EUR
 plink-1.9 --bfile EUR --freq --out EUR
 awk -vOFS="\t" '(NR>1){print $2,$5}' EUR.frq > EUR.dat
-stata <<END
-  insheet rsid FreqA2 using EUR.dat, case
-  sort rsid
-  gzsave EUR, replace
-  insheet chr rsid m pos A1 A2 using EUR.bim, case clear
-  gen RSnum=rsid
-  gen info=1
-  gen type=2
-  sort rsid
-  gzmerge using EUR
-  gen snpid=string(chr)+":"+string(pos,"%12.0f")+"_"+cond(A1<A2,A1,A2)+"_"+cond(A1<A2,A2,A1)
-  sort chr pos
-  drop _merge
-  gen maf=cond(FreqA2<=0.5, FreqA2, 1-FreqA2)
-  sort chr pos snpid
-  gen MAC=2*489*maf
-  rename FreqA2 exp_freq_a1
-  order snpid pos exp_freq_a1 info type
-  gzsave FUSION, replace
-END
+
+stata -b do FUSION.do
